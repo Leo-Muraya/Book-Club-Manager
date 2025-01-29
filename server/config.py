5323785bdab8873c974ad8eaec4ac23cd27bf76b
Leguminous <murayaleo911@gmail.com>
2025-01-29 19:52:55 +0300

    made routes

--- server/config.py
+++ server/config.py
@@ -1,22 +1,23 @@
-# Standard library imports
-
-# Remote library imports
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+import os
 
-# Local imports
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///book_club.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'jwtsecret')
 
-# Instantiate app, set attributes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-# Define metadata, instantiate db
+
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
@@ -24,8 +25,7 @@ db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
-# Instantiate REST API
+
 api = Api(app)
 
-# Instantiate CORS
 CORS(app)
---
+++ server/routes/authentification.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import create_access_token
+from models import db, User
+
+auth_bp = Blueprint('auth', __name__)
+bcrypt = Bcrypt()
+
+@auth_bp.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(username=data['username'], email=data['email'], password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(message='User registered successfully'), 201
+
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    user = User.query.filter_by(email=data['email']).first()
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        access_token = create_access_token(identity=user.id)
+        return jsonify(access_token=access_token), 200
+    return jsonify(message='Invalid credentials'), 401
