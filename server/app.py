440279a0f0650919ec8d69e483f979cf63e0ee4b
Leguminous <murayaleo911@gmail.com>
2025-01-29 19:07:51 +0300

    installed dependancies

--- server/app.py
+++ server/app.py
@@ -2,20 +2,28 @@
 
 # Standard library imports
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
+db.init_app(app)
 
-# Views go here!
+api = Api(app)
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+CORS(app)
 
 
 if __name__ == '__main__':
